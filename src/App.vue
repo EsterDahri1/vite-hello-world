4bdec1eb1167889c7eaf9e0acce6f45fcfc577a1
EsterDahri <esterdahri@gmail.com>
2023-09-21 16:13:21 +0200

    Delete helloworld and add AppTitle.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
+<!--
+Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell'installazione come visti a lezione.✔️
+Modificate il componente App.vue, che deve usare l'option api, e poi nel template stampate in pagina un titolo hello Vite ✔️
+Bonus:
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!" ✔️
+Creare un secondo componente AppHero, che visualizza un jumbotron.-->
+
 <script>
+import AppTitle from './components/AppTitle.vue';
+
+
 export default {
   data() {
     return {
       message: 'Hello Vite!'
-    }
-  }
+    };
+  },
+  components: { AppTitle }
 }
 </script>
 
 <template>
+  <AppTitle />
   <h1>{{ message }}</h1>
 </template>
 
